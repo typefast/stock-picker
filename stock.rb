530f5aa1ad4444e781568b76c3b9d78818a62374
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-14 15:25:41 +0000

    iterate through array with value and index

---
+++ stock.rb
@@ -0,0 +1,16 @@
+#array of stock prices
+#return a pair of days best day to buy and best to sell
+#days start at 0
+#buy first then sell
+def stock_picker(array)
+  
+  array.each_with_index do |day, index|
+    
+  end
+  
+end
+#check the difference between the numbers in an array
+#if the next day is greater go to the next
+#if the next day is less take the current day and deduct from starting day for profit
+stock_picker([17,3,6,9,15,8,6,1,10])
+
